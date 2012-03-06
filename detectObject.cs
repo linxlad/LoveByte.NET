84509ad1a61f0dad1f614a80eea44c8aa0e273e6
Nathan Daly <justlikephp@gmail.com>
2012-03-06 02:52:07 +0000

    Update detectObject.cs

--- detectObject.cs
+++ detectObject.cs
@@ -44,18 +44,18 @@ namespace detectObject
             hslFilter.Saturation = new AForge.DoubleRange(0.7, 1);
             hslFilter.Luminance = new AForge.DoubleRange(0.2, 0.7);
 
-			//Instantiate new RGB filter 
+	    //Instantiate new RGB filter 
             /*AForge.Imaging.Filters.ColorFiltering rgbFilter = new AForge.Imaging.Filters.ColorFiltering();
 			
-			//Define RGB colour range (default colour is orange)
+	    //Define RGB colour range (default colour is orange)
             rgbFilter.Red = new AForge.IntRange(60, 180);
             rgbFilter.Green = new AForge.IntRange(10, 80);
             rgbFilter.Blue = new AForge.IntRange(5, 25);*/
 
-			//Apply the HSL filter to the input image
+	    //Apply the HSL filter to the input image
             hslFilter.ApplyInPlace(image);
 			
-			//Create a 5 by 5 matrix using a short array
+	    //Create a 5 by 5 matrix using a short array
             int rows = 5;
             int cols = 5;
             short[,] Matrix = new short[rows, cols];
@@ -79,37 +79,37 @@ namespace detectObject
         /// <returns>Rectangle</returns>
         public Rectangle DrawRectangle(Bitmap image)
         {
-			//Instantiate new blob counter
+	    //Instantiate new blob counter
             AForge.Imaging.BlobCounter detectBlob = new AForge.Imaging.BlobCounter();
 
             //Get object by size
             detectBlob.ObjectsOrder = AForge.Imaging.ObjectsOrder.Size;
 			
-			//Apply BlobCounter
+	    //Apply BlobCounter
             detectBlob.ProcessImage(image);
 
             //objects in an array.0
             Rectangle[] rects = detectBlob.GetObjectsRectangles();
 			
-			//Create graphics from image
+	    //Create graphics from image
             Graphics g = Graphics.FromImage(image);
 
             try
             {
-				//Draw rectangle around largest rectangle (default colour is red)
+		//Draw rectangle around largest rectangle (default colour is red)
                 g.DrawRectangle(new Pen(Color.Red), rects[0]);
 				
-				//Optional to save the result image to the startup directory
+		//Optional to save the result image to the startup directory
                 //string startupPath = System.IO.Directory.GetCurrentDirectory();
                 //image.Save(startupPath + @"\nameyourfile.jpg");
 				
-				//Return the rantngla
+		//Return the rantngla
                 return rects[0];
 
             }
             catch (IndexOutOfRangeException e)
             {
-				//If no blobs return empty rectangle
+		//If no blobs return empty rectangle
                 Rectangle rrr = new Rectangle(0, 0, 0, 0);
                 return rrr;
             }
